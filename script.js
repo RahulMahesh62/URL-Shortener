a8c4790c4f8d2bce88d9977212a4c8bc1bc1b347
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-11-03 17:24:45 +0530

    Completed project

---
+++ script.js
@@ -0,0 +1,33 @@
+const express = require('express');
+var shorturl = require('shorturl');
+var TinyURL = require('tinyurl');
+var ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+
+app.post("/", function(req, res) {
+	var url = req.body.url;
+	url = url.toString();
+	var resurl = "";
+	TinyURL.shorten(url, function(result, err) {
+  	if (err)
+    	console.log(err)
+	else
+		resurl = result.toString();
+		res.render("result", { opresult: resurl });
+	});
+});
+
+app.listen(4000,()=> {
+	console.log("Server Started");
+});
